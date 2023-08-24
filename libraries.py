65aeca2add8dec2588b6b4a24ffe6c0daafecad9
francescomagnani <75760916+francescomagnani@users.noreply.github.com>
2023-08-24 17:57:21 +0200

    libraries.py

--- libraries.py
+++ libraries.py
@@ -5,18 +5,16 @@ import subprocess
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'argparse'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'tomli==2.0.1'])
 # optimization script
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'astropy==5.1'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'numpy==1.23.5'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pandas==2.0.0'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'datetime'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'matplotlib==3.6.0'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'healpy'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'ligo.skymap==1.0.6'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'utm'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'h5py'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'numexpr'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'tables'])
-# skymaps
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'matplotlib==3.6.0'])
 
 # folders
 os.mkdir("datasets")
